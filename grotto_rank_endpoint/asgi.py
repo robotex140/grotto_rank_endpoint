24590c2cf93fd597b91373d0f612867fbe52bcf3
Robotex140 <57817292+robotex140@users.noreply.github.com>
2023-06-17 23:02:44 +0300

    initial commit

---
+++ grotto_rank_endpoint/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for grotto_rank_endpoint project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grotto_rank_endpoint.settings')
+
+application = get_asgi_application()
